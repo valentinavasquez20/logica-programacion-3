25b05ba165712681a3221dfd0ffdcbf966731530
valentinavasquez20 <155574285+valentinavasquez20@users.noreply.github.com>
2024-01-28 02:12:11 -0300

    commitFinal

--- logicaprog3.js
+++ logicaprog3.js
@@ -1,19 +1,30 @@
-function CalcularFact() {
-    // Obtiene el número ingresado por el usuario
-    const numero = document.getElementById("numero").value;
-  
+const calcular = document.getElementById("calcular");
+const resultado = document.getElementById("resultado");
+const numeroInput = document.getElementById("numero"); // Obtener la referencia al campo de texto
 
-    //if (!Number.type(numero)) {
-    //  alert("El dato ingresado debe ser un número entero.");
-    //  return;
-    //}
-  
-    // Calcula el factorial del número
-    let factorial = 1;
-    for (let i = numero; i > 0; i--) {
-      factorial *= i;
-    }
-  
-    // Imprime el resultado en el DOM
-    document.querySelector("resultado").innerHTML = factorial;
+const calcularFactorial = (numero) => {
+  let factorial = 1;
+
+  for (let i = numero; i > 0; i--) {
+    factorial *= i;
   }
+
+  return factorial;
+};
+
+if (calcular) {
+  calcular.addEventListener("click", () => {
+    let numeroIngresado = numeroInput.value; // Obtener el valor del campo de texto
+
+    let numeroFloat = parseFloat(numeroIngresado);
+
+    if (isNaN(numeroFloat)) {
+      resultado.textContent = "El dato de entrada no es un número válido.";
+      return;
+    }
+
+    let numero = numeroIngresado;
+
+    resultado.textContent = `El factorial del numero ${numero} es: ${calcularFactorial(numero)}`;
+  });
+}
